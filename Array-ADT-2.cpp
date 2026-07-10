0e4f86275f6fe2d02603e4fa9a788a207c3979bf
Shantanu Kulkarni <kulshan0404@gmail.com>
2026-07-10 19:50:24 +0530

    A static array, to easily show changes to the array and operations performed without any problems, handling calls by address as well as by values

---
+++ Array-ADT-2.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <stdlib.h>
+#include<stdio.h>
+struct Array
+{
+ int A[10];
+ int size;
+ int length;
+};
+void Append(struct Array *arr,int x)
+ {
+ if(arr->length<arr->size)
+ arr->A[arr->length++]=x;
+ }
+ void Insert(struct Array *arr,int index,int x)
+ {
+ int i;
+
+ if(index>=0 && index <=arr->length)
+ {
+ for(i=arr->length;i>index;i--)
+ arr->A[i]=arr->A[i-1];
+ arr->A[index]=x;
+ arr->length++;
+ }
+ } 
+void Display(struct Array arr)
+ {
+ int i;
+ printf("\nElements are\n");
+ for(i=0;i<arr.length;i++)
+ printf("%d ",arr.A[i]);
+ }
+int Delete(struct Array *arr,int index)
+{
+ int x=0;
+ int i;
+ if(index>=0 && index<arr->length)
+ {
+ x=arr->A[index];
+ for(i=index;i<arr->length-1;i++)
+ arr->A[i]=arr->A[i+1];
+ arr->length--;
+ return x;
+ }
+ return 0;
+}
+void swap(int *x, int *y){
+    int temp = *x;
+    *x=*y;
+    *y=temp;
+}
+int LinearSearch(struct Array *  arr, int key){
+    int i;
+    for(i=0;i<arr->length; i++){
+        if(key == arr->A[i]){
+            swap(&arr->A[i], &arr->A[i-1]);
+            return i-1;
+        }
+    }
+            return -1;
+}
+int main()
+{
+ struct Array arr={{2,3,4,5,6},10,5};
+ Append(&arr,10);
+ Insert(&arr,0,12);
+ printf("%d", LinearSearch(&arr, 4));
+ Display(arr);
+ return 0;
+}
