83fbcfeda6b1a41c42ccba9b9f683e2b0a32bf60
Shantanu Kulkarni <kulshan0404@gmail.com>
2026-07-15 15:31:42 +0530

    Added binary search in regular and recursive ways, to put to use, disable by adding double slash comments in front of insert and linear search to disable messing with the sorted array, as binary search is performed only on sorted arrays.

--- Array-ADT-2.cpp
+++ Array-ADT-2.cpp
@@ -60,12 +60,47 @@ int LinearSearch(struct Array *  arr, int key){
     }
             return -1;
 }
+int BinSearch(struct Array arr, int key){
+    int l, mid, h;
+    l=0, h=arr.length-1;
+    while(l<=h){
+        mid = (l+h)/2;
+        if(key == arr.A[mid]){
+            return mid;
+        }
+        else if(key < arr.A[mid]){
+            h = mid - 1;
+        }
+        else{
+            l = mid + 1;
+        }
+    }
+    return -1;
+}
+int RecBinSearch(struct Array *arr, int l, int h, int key){
+    static int mid;
+    if(l<=h){
+        mid = (l+h)/2;
+        if(key == arr->A[mid]){
+            return mid;
+        }
+        else if(key < arr->A[mid]){
+            return RecBinSearch(arr, l, mid-1, key);
+        }
+        else{
+            return RecBinSearch(arr, mid+1, h, key);
+        }
+    }
+    return -1;
+}
 int main()
 {
  struct Array arr={{2,3,4,5,6},10,5};
  Append(&arr,10);
  Insert(&arr,0,12);
  printf("%d", LinearSearch(&arr, 4));
+ printf("%d\n", BinSearch(arr, 69));
+ printf("%d", RecBinSearch(&arr, 0, arr.length-1, 4));
  Display(arr);
  return 0;
 }
