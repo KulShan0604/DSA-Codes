6e1d8733b2edac35f3d87eabac221042d6bda909
Shantanu Kulkarni <kulshan0404@gmail.com>
2026-07-17 14:33:32 +0530

    Addition of some basic working functions, like get set max min etc.

--- Array-ADT-2.cpp
+++ Array-ADT-2.cpp
@@ -93,11 +93,56 @@ int RecBinSearch(struct Array *arr, int l, int h, int key){
     }
     return -1;
 }
+int get(struct Array arr, int index){
+    if(index>=0 && index<arr.length){
+        return arr.A[index];
+    }
+    return -1;
+}
+void set(struct Array *arr, int index, int x){
+    if(index>=0 && index<arr->length){
+        arr->A[index] = x;
+    }
+}
+int max(struct Array arr){
+    int max = arr.A[0];
+    int i;
+    for(i=1; i<arr.length; i++){
+        if(arr.A[i]>max){
+            max = arr.A[i];
+        }
+    }
+    return max;
+}
+int min(struct Array arr){
+     int min = arr.A[0];
+    int i;
+    for(i=1; i<arr.length; i++){
+        if(arr.A[i]<min){
+            min = arr.A[i];
+        }
+    }
+    return min;
+}
+int sum(struct Array arr){
+    int s=0;
+    for(int i=0; i<arr.length; i++){
+        s = s + arr.A[i];
+    }
+    return s;
+}
+float avg(struct Array arr){
+    return (float)sum(arr) / arr.length;
+}
 int main()
 {
  struct Array arr={{2,3,4,5,6},10,5};
  Append(&arr,10);
  Insert(&arr,0,12);
+ printf("%d\n", get(arr, 3));
+ printf("%d\n", max(arr));
+ printf("%d\n", min(arr));
+ printf("%f\n", avg(arr));
  printf("%d", LinearSearch(&arr, 4));
  printf("%d\n", BinSearch(arr, 69));
  printf("%d", RecBinSearch(&arr, 0, arr.length-1, 4));
