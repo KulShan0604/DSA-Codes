5a44d1b61b5d7385f500a890292f18d5480004d5
Shantanu Kulkarni <kulshan0404@gmail.com>
2026-07-18 11:10:12 +0530

    Added a few operations today, reversing a list, left shift etc. Worked on the array a bit, separating negatives and positives in an array, checking if it is sorted and more.

--- Array-ADT-2.cpp
+++ Array-ADT-2.cpp
@@ -134,6 +134,61 @@ int sum(struct Array arr){
 float avg(struct Array arr){
     return (float)sum(arr) / arr.length;
 }
+void reverse(struct Array *arr){
+    int *B;
+    int i, j;
+    B = new int[arr->length]; 
+    for(i=arr->length-1, j=0; i>=0; i--, j++){
+        B[j]=arr->A[i];
+    }
+    for(i=0; i<arr->length; i++){
+        arr->A[i]=B[i];
+    }
+}
+void reverse2(struct Array *arr){
+    int i, j;
+    for(i=0, j=arr->length-1; i<j; i++, j--){
+        swap(&arr->A[i], &arr->A[j]);
+    }
+}
+void InsertSort(struct Array *arr, int x){
+    if(arr->length==arr->size){
+        return;
+    }
+    int i = arr->length-1;
+    while(i>=0 && arr->A[i]>x){
+        arr->A[i+1] = arr->A[i];
+        i--;
+    }
+    arr->A[i+1] = x;
+    arr->length++; 
+}
+int isSorted(struct Array arr){
+    int i;
+    for(i=0; i<arr.length-1; i++){
+        if(arr.A[i]>arr.A[i+1]){
+            return 0;
+        }
+    }
+    return 1;
+}
+void rearrange(struct Array *arr){
+    int i, j;
+    i = 0;
+    j = arr->length-1;
+    while(i<j){
+        while(arr->A[i]<0){
+            i++;
+        }
+        while(arr->A[j]>=0){
+            j--;
+        }
+        if(i<j){
+            swap(&arr->A[i], &arr->A[j]);
+        }
+        
+    }
+}
 int main()
 {
  struct Array arr={{2,3,4,5,6},10,5};
@@ -146,6 +201,11 @@ int main()
  printf("%d", LinearSearch(&arr, 4));
  printf("%d\n", BinSearch(arr, 69));
  printf("%d", RecBinSearch(&arr, 0, arr.length-1, 4));
+ reverse(&arr);
+ reverse2(&arr);
+ InsertSort(&arr, 3.5);
+ rearrange(&arr);
+ printf("%d\n", isSorted(arr));
  Display(arr);
  return 0;
 }
